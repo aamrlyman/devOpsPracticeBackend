9e8f007a8f380c51531ff808544f71f5b9c74021
aamrlyman <aamrlyman@gmail.com>
2023-03-13 20:06:08 -0700

    reconfigure for EB

--- backend/music_library/settings.py
+++ backend/music_library/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-j((u)ws9)^d_sbga^cizvr%ged3d2d0t46*9=5d$)al(s@v_6c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['devOpsPracticeBackend-env.eba-jp62r3fs.us-west-2.elasticbeanstalk.com', '127.0.0.1', 'devOpsPracticeBackend2-env.eba-aqhajsas.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
